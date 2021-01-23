c15e63cd4b64ff9d4a5f6462b2f0a3d2b4bd6acf
JLarkin99 <jlarkin2109@gmail.com>
2021-01-23 16:02:32 +0000

    Main activities added

---
+++ app/src/main/java/com/example/mpacoursework/cookActivityMain.java
@@ -0,0 +1,14 @@
+package com.example.mpacoursework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cookActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_main);
+    }
+}
