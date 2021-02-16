ef33ffcb558c0e72e23e1098efd5ab0e6ac9129d
JLarkin99 <jlarkin2109@gmail.com>
2021-02-16 12:49:17 +0000

    Search Bar base added

--- app/src/main/java/com/example/mpacoursework/cookActivityMain.java
+++ app/src/main/java/com/example/mpacoursework/cookActivityMain.java
@@ -1,14 +1,58 @@
 package com.example.mpacoursework;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class cookActivityMain extends AppCompatActivity {
-
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cook_main);
+        
+        ListView listView = findViewById(R.id.cookSearchBar);
+        List<String> cookList = new ArrayList<>();
+        //prototype, change later
+        cookList.add("Spaghetti");
+        cookList.add("burger");
+        cookList.add("Crumbs");
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1 , cookList);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.cook_search_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_icon);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search Here!");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
